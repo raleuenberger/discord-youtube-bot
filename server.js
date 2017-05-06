fd55a3addeb052c1d956e22db76bd732f06af3a8
DESKTOP-EBVLFNS\Robert Leuenberger <raleuenberger@gmail.com>
2017-05-05 21:33:15 -0500

    connects to channel, checks channel for input, reads client/token information from private file

--- server.js
+++ server.js
@@ -4,6 +4,22 @@
 
 const Discord = require('discord.js');
 
-const hook = new Discord.WebhookClient('id', 'token');
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
-hook.send('I am now alive!');
+const client = new Discord.Client();
+const token = fs.readFileSync("private/token.txt", "utf8");
+
+client.on('ready', function() {
+    console.log("Youtube Audio Bot Activated!")
+});
+
+client.on('guildMemberAdd', function(member) {
+    member.guild.defaultChannel.send('HEY YOU! ${member}! WELCOME!');
+});
+
+client.on('message', function(message) {
+    message.channel.send('BLOOP!');
+});
+
+client.login(token);
