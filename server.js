2eb1a3dcc51c9ef5bfb7138168430adbfea78c7e
Tarczon <tarczon_24@hotmail.com>
2017-05-06 12:13:45 -0500

    audio playback working

--- server.js
+++ server.js
@@ -15,29 +15,37 @@ const commands = {
 	"play": {
 		usage: "!play <youtube_url> [voice_channel]",
 		description: "Plays youtube links for a voice channel; if no voice channel is specified, it will default to the 'general' channel",
-		process: function(bot,args){
+		process: function(bot,msg,args){
 			var videoLink = args[0];
 			var options = args.slice(1,args.length);
+
+			const streamOptions = { seek: 0, volume: 1 };
+			const broadcast = bot.createVoiceBroadcast();
+
+			var channel = getVoiceChannel(bot, msg);
+
+			channel.join()
+				.then(function(connection) {
+					const stream = ytdl(videoLink, { filter: 'audioonly'});
+					broadcast.playStream(stream);
+					const dispatcher = connection.playBroadcast(broadcast);
+				})
+				.catch(console.error);
+		}
+	},
+	"stop": {
+		usage: "!stop",
+		description: "Ends playback.",
+		process: function(bot,msg,args){
+			bot.broadcasts.forEach(function(broadcast){
+				broadcast.end();
+			})
 		}
 	}
 };
 
 bot.on('ready', function() {
-    console.log("Youtube Audio Bot Activated!")
-    //console.log(client.createVoiceBroadcast);
-
-    const streamOptions = { seek: 0, volume: 1 };
-    const broadcast = client.createVoiceBroadcast();
-
-    var channel = client.channels.find('name', 'General');
-
-    channel.join()
-     .then(function(connection) {
-         const stream = ytdl('https://www.youtube.com/watch?v=SW-BU6keEUw', { filter: 'audioonly'});
-         broadcast.playStream(stream);
-         const dispatcher = connection.playBroadcast(broadcast);
-     })
-     .catch(console.error);
+    console.log("Youtube Audio Bot Activated!");
 });
 
 bot.on('guildMemberAdd', function(member) {
@@ -45,10 +53,14 @@ bot.on('guildMemberAdd', function(member) {
 });
 
 bot.on('message', function( message ) {
-    if( (acceptableMessages.indexOf( message.content ) > -1 ) && !message.author.bot ) {
-        message.channel.send( 'Suhhh' );
-	}
+    parseText(message);
 } );
+
+function getVoiceChannel(bot, msg) {
+	var userId = msg.author.id;
+	var guild = msg.channel.guild;
+	return bot.channels.find("id",guild.members.find("id",userId).voiceChannelID);
+}
 
 function isBotMessage(message) {
 	return message.author.bot;
@@ -69,17 +81,30 @@ function parseText(text) {
 }
 
 function parseBotCommand(command) {
-	var tokens = command.split(" ");
-	var cmd = tokens[0].substring(cmdPrefix.length);
+	var tokens = command.content.split(" ");
+	var cmdTxt = tokens[0].substring(cmdPrefix.length);
 	var args = tokens.slice(1,tokens.length);
 
-
+	var cmd = commands[cmdTxt];
+	if(cmd) {
+		try{
+			cmd.process(bot, command, args);
+		} catch(e){
+			console.log("cmd " + cmdTxt + " failed! \nStack Trace: \n" + e.stack);
+			command.channel.send("command " + cmdTxt + " failed!");
+		}
+	} else {
+		command.channel.send(cmdTxt + " not recognized as a command!").then((message => message.delete(10000)));
+	}
 }
 
 function parseMessage(message) {
-	
+	if(message.content === 'hi')
+		message.channel.send('Why hello there');
+	else if (message.content === 'what is my avatar?')
+		message.reply(message.author.avatarURL);
 }
 
 
-client.login(token);
+bot.login(token);
 
